c065b004e5ee9cf0f79138cc76c8ffc4284743fb
Aidas Klimas <aidaskk@gmail.com>
2013-09-13 17:51:03 +0300

    feat: helper methods and own exception class

---
+++ src/Resumable/Exception.php
@@ -0,0 +1,7 @@
+<?php
+namespace Resumable;
+
+class Exception extends \Exception
+{
+
+}
--- src/Resumable/Uploader.php
+++ src/Resumable/Uploader.php
@@ -3,4 +3,46 @@ namespace Resumable;
 
 class Uploader {
 
+    /**
+     * Delete chunks older than expiration time.
+     * @param string $chunksFolder
+     * @param int $expirationTime seconds
+     * @throws Exception
+     */
+    public static function pruneChunks($chunksFolder, $expirationTime = 172800)
+    {
+        $handle = opendir($chunksFolder);
+        if (!$handle) {
+            throw new Exception('Failed to open folder');
+        }
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry == "." || $entry == "..") {
+                continue;
+            }
+            $path = $chunksFolder . DIRECTORY_SEPARATOR . $entry;
+            if (!is_dir($path)) {
+                continue;
+            }
+            if (time() - filemtime($path) > $expirationTime) {
+                self::deleteChunksDirectory($path);
+            }
+        }
+        closedir($handle);
+    }
+
+    /**
+     * Helper method for folder deletion
+     * @param $path
+     * @return bool
+     */
+    public static function deleteChunksDirectory($path)
+    {
+        foreach (scandir($path) as $item) {
+            if ($item == "." || $item == "..") {
+                continue;
+            }
+            unlink($path . DIRECTORY_SEPARATOR . $item);
+        }
+        return rmdir($path);
+    }
 }

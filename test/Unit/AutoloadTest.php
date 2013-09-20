14a22262dd83309d0ca2d20ca7dfe4fd10eaeef5
Aidas Klimas <aidaskk@gmail.com>
2013-09-20 16:43:37 +0300

    test(Autoload): autoloader tests improved

--- test/Unit/AutoloadTest.php
+++ test/Unit/AutoloadTest.php
@@ -6,6 +6,12 @@ class AutoloadTest extends \PHPUnit_Framework_TestCase
 {
     public function testClassesExist()
     {
+        $autoloader = new \Resumable\Autoloader();
+        $autoloader->autoload('noclass');
+        $this->assertFalse(class_exists('noclass', false));
+        $autoloader->autoload('Resumable\NoClass');
+        $this->assertFalse(class_exists('Resumable\NoClass', false));
+        $autoloader->autoload('Resumable\File');
         $this->assertTrue(class_exists('Resumable\File'));
     }
 }
